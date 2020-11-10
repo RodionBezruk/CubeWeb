23da598fa733a1c87316566ef4697fa333cc26ff
Rodion <rodion.bezruk@devit-team.com>
2020-11-10 00:36:32 +0000

    LegacyRender version 1.07

--- html/modules/legacyRender/xoops_version.php
+++ html/modules/legacyRender/xoops_version.php
@@ -1,6 +1,6 @@
 <?php
 $modversion['name']=_MI_LEGACYRENDER_NAME;
-$modversion['version'] = 1.06; 
+$modversion['version']=1.07;
 $modversion['description']=_MI_LEGACYRENDER_NAME_DESC;
 $modversion['author']="";
 $modversion['credits']="The XOOPS Cube Project";
@@ -19,7 +19,7 @@ $modversion['config'][]=array (
 		"description"=>"_MI_LR_META_KEYWORDS_DESC",
 		"formtype"=>"textarea",
 		"valuetype"=>"text",
-		"default"=>"news, technology, headlines, xoops, xoop, nuke, myphpnuke, myphp-nuke, phpnuke, SE, geek, geeks, hacker, hackers, linux, software, download, downloads, free, community, mp3, forum, forums, bulletin, board, boards, bbs, php, survey, poll, polls, kernel, comment, comments, portal, odp, open, source, opensource, FreeSoftware, gnu, gpl, license, Unix, *nix, mysql, sql, database, databases, web site, weblog, guru, module, modules, theme, themes, cms, content management"
+		"default"=>"news, technology, headlines, xoops, cube, legacy, social, network, module, plugin, theme, css, web 2.0, ajax, cms, content management, nuke, myphpnuke, myphp-nuke, phpnuke, SE, geek, geeks, hacker, hackers, linux, software, download, downloads, free, community, mp3, forum, forums, bulletin, board, boards, bbs, php, survey, poll, polls, kernel, comment, comments, portal, odp, open, source, opensource, FreeSoftware, gnu, gpl, license, Unix, *nix, mysql, sql, database, databases, web site, weblog, guru"
 	);
 $modversion['config'][]=array (
 		"name"=>"meta_description",
@@ -27,7 +27,7 @@ $modversion['config'][]=array (
 		"description"=>"_MI_LR_META_DESCRIPTION_DESC",
 		"formtype"=>"textarea",
 		"valuetype"=>"text",
-		"default"=>"XOOPS is a dynamic Object Oriented based open source portal script written in PHP."
+		"default"=>"XOOPS Cube Legacy is a dynamic Object Oriented based open source portal script written in PHP."
 	);
 $modversion['config'][]=array (
 		"name"=>"meta_robots",
@@ -61,7 +61,7 @@ $modversion['config'][]=array (
 		"description"=>"_MI_LR_META_COPYRIGHT_DESC",
 		"formtype"=>"textbox",
 		"valuetype"=>"text",
-		"default"=>"Copyright &copy; 2001-2006"
+		"default"=>"Copyright &copy; 2001-2008"
 	);
 $modversion['config'][]=array (
 		"name"=>"footer",
@@ -69,7 +69,7 @@ $modversion['config'][]=array (
 		"description"=>"_MI_LR_FOOTER_DESC",
 		"formtype"=>"textarea",
 		"valuetype"=>"text",
-		"default"=>"Powered by XOOPS Cube 2.1&copy; 2001-2006 <a href=\"http:
+		"default"=>"Powered by <a href=\"http:
 	);
 $modversion['config'][]=array (
 		"name"=>"banners",
